c72f4bd2159d1405bae8c6f61f08e1de7c4e26e0
MDJAmin <mohammadamindf@gmail.com>
2024-06-02 15:52:35 +0430

    Add

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+function calculate() {
+  var num1 = parseFloat(document.querySelector("#num1").value);
+  var num2 = parseFloat(document.querySelector("#num2").value);
+  var operator = document.querySelector("#operator").value;
+  var result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+      case "**":
+        result = num1 ** num2;
+        break;
+    default:
+      result = "Unauthorized operation!";
+  }
+
+  document.querySelector("#result").value = result;
+  document.querySelector("")
+}
